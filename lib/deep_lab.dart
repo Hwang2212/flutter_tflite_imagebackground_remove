c109b929f6ee1545a9292b1dcf676a64dd9c9e2d
Andy Hwang <andyhwang@Andys-MacBook-Pro.local>
2023-01-28 16:21:50 +0800

    Testing

--- lib/deep_lab.dart
+++ lib/deep_lab.dart
@@ -1,67 +1,67 @@
-import 'dart:developer';
-import 'dart:io';
-import 'dart:ui';
+// import 'dart:developer';
+// import 'dart:io';
+// import 'dart:ui';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter_tflite/flutter_tflite.dart';
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter_tflite/flutter_tflite.dart';
 
-class DeepLab {
-  static const String deeplabv3 = "DeepLabv3";
+// class DeepLab {
+//   static const String deeplabv3 = "DeepLabv3";
 
-  static void loadModel() async {
-    Tflite.close();
-    try {
-      String? res;
-      res = await Tflite.loadModel(
-          // model: 'assets/tflite/deeplabv3_257_mv_gpu.tflite',
-          model: 'assets/tflite/deep.tflite',
-          labels: 'assets/tflite/deeplabv3_257_mv_gpu.txt');
-      log("YAY");
-    } catch (e) {
-      log(e.toString());
-    }
-  }
+//   static void loadModel() async {
+//     Tflite.close();
+//     try {
+//       String? res;
+//       res = await Tflite.loadModel(
+//           // model: 'assets/tflite/deeplabv3_257_mv_gpu.tflite',
+//           model: 'assets/tflite/deep.tflite',
+//           labels: 'assets/tflite/deeplabv3_257_mv_gpu.txt');
+//       log("YAY");
+//     } catch (e) {
+//       log(e.toString());
+//     }
+//   }
 
-  static Future<Uint8List> predictImage(File image) async {
-    Uint8List imaged = await performSegmentation(image);
-    return imaged;
-  }
+//   static Future<Uint8List> predictImage(File image) async {
+//     Uint8List imaged = await performSegmentation(image);
+//     return imaged;
+//   }
 
-  static List<int> newcolor = [
-    //  Change color here
-    const Color.fromARGB(0, 0, 0, 0).value, // background
-    const Color.fromARGB(255, 128, 0, 0).value, // aeroplane
-    const Color.fromARGB(255, 0, 128, 0).value, // biyclce
-    const Color.fromARGB(255, 128, 128, 0).value, // bird
-    const Color.fromARGB(255, 0, 0, 128).value, // boat
-    const Color.fromARGB(255, 128, 0, 128).value, // bottle
-    const Color.fromARGB(255, 0, 128, 128).value, // bus
-    const Color.fromARGB(255, 128, 128, 128).value, // car
-    const Color.fromARGB(255, 64, 0, 0).value, // cat
-    const Color.fromARGB(255, 192, 0, 0).value, // chair
-    const Color.fromARGB(255, 64, 128, 0).value, // cow
-    const Color.fromARGB(255, 192, 128, 0).value, // diningtable
-    const Color.fromARGB(255, 64, 0, 128).value, // dog
-    const Color.fromARGB(255, 192, 0, 128).value, // horse
-    const Color.fromARGB(255, 64, 128, 128).value, // motorbike
-    const Color.fromARGB(255, 0, 0, 0).value, // person
-    const Color.fromARGB(255, 0, 64, 0).value, // potted plant
-    const Color.fromARGB(255, 128, 64, 0).value, // sheep
-    const Color.fromARGB(255, 0, 192, 0).value, // sofa
-    const Color.fromARGB(255, 128, 192, 0).value, // train
-    const Color.fromARGB(255, 0, 64, 128).value, // tv-monitor
-  ];
+//   static List<int> newcolor = [
+//     //  Change color here
+//     const Color.fromARGB(0, 0, 0, 0).value, // background
+//     const Color.fromARGB(255, 128, 0, 0).value, // aeroplane
+//     const Color.fromARGB(255, 0, 128, 0).value, // biyclce
+//     const Color.fromARGB(255, 128, 128, 0).value, // bird
+//     const Color.fromARGB(255, 0, 0, 128).value, // boat
+//     const Color.fromARGB(255, 128, 0, 128).value, // bottle
+//     const Color.fromARGB(255, 0, 128, 128).value, // bus
+//     const Color.fromARGB(255, 128, 128, 128).value, // car
+//     const Color.fromARGB(255, 64, 0, 0).value, // cat
+//     const Color.fromARGB(255, 192, 0, 0).value, // chair
+//     const Color.fromARGB(255, 64, 128, 0).value, // cow
+//     const Color.fromARGB(255, 192, 128, 0).value, // diningtable
+//     const Color.fromARGB(255, 64, 0, 128).value, // dog
+//     const Color.fromARGB(255, 192, 0, 128).value, // horse
+//     const Color.fromARGB(255, 64, 128, 128).value, // motorbike
+//     const Color.fromARGB(255, 0, 0, 0).value, // person
+//     const Color.fromARGB(255, 0, 64, 0).value, // potted plant
+//     const Color.fromARGB(255, 128, 64, 0).value, // sheep
+//     const Color.fromARGB(255, 0, 192, 0).value, // sofa
+//     const Color.fromARGB(255, 128, 192, 0).value, // train
+//     const Color.fromARGB(255, 0, 64, 128).value, // tv-monitor
+//   ];
 
-  static Future<Uint8List> performSegmentation(File image) async {
-    Uint8List? recognition = await Tflite.runSegmentationOnImage(
-      path: image.path,
-      imageMean: 0.0,
-      imageStd: 255.0,
-      outputType: "png",
-      labelColors: newcolor,
-      asynch: true,
-    );
+//   static Future<Uint8List> performSegmentation(File image) async {
+//     Uint8List? recognition = await Tflite.runSegmentationOnImage(
+//       path: image.path,
+//       imageMean: 0.0,
+//       imageStd: 255.0,
+//       outputType: "png",
+//       labelColors: newcolor,
+//       asynch: true,
+//     );
 
-    return recognition!;
-  }
-}
+//     return recognition!;
+//   }
+// }
--- lib/home.dart
+++ lib/home.dart
@@ -1,145 +1,144 @@
-import 'dart:io';
-import 'dart:ui';
+// import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_application_1/deep_lab.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:widget_mask/widget_mask.dart';
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/deep_lab.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:widget_mask/widget_mask.dart';
 
-import 'image_picker_service.dart';
+// import 'image_picker_service.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+// class Home extends StatefulWidget {
+//   const Home({super.key});
 
-  @override
-  State<Home> createState() => _HomeState();
-}
+//   @override
+//   State<Home> createState() => _HomeState();
+// }
 
-class _HomeState extends State<Home> {
-  NavigatorState get navigatorState => Navigator.of(context);
-  File? _profilePic;
-  Uint8List? finalPic;
-  double? _imageWidth;
-  double? _imageHeight;
+// class _HomeState extends State<Home> {
+//   NavigatorState get navigatorState => Navigator.of(context);
+//   File? _profilePic;
+//   Uint8List? finalPic;
+//   double? _imageWidth;
+//   double? _imageHeight;
 
-  @override
-  void initState() {
-    super.initState();
-    DeepLab.loadModel();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     DeepLab.loadModel();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              buildProfilePicture(),
-              buildCameraButton(),
-              buildGalleryButton(),
-              // buildFinalPic(),
-              buildStackImage()
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: SingleChildScrollView(
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               buildProfilePicture(),
+//               buildCameraButton(),
+//               buildGalleryButton(),
+//               // buildFinalPic(),
+//               buildStackImage()
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
 
-  buildProfilePicture() {
-    if (_profilePic != null) {
-      return Image.file(
-        _profilePic!,
-        fit: BoxFit.fill,
-        height: 500,
-        // width: 300,
-      );
-    }
-    return const Icon(
-      Icons.camera,
-      size: 200,
-    );
-  }
+//   buildProfilePicture() {
+//     if (_profilePic != null) {
+//       return Image.file(
+//         _profilePic!,
+//         fit: BoxFit.fill,
+//         height: 500,
+//         // width: 300,
+//       );
+//     }
+//     return const Icon(
+//       Icons.camera,
+//       size: 200,
+//     );
+//   }
 
-  buildFinalPic() {
-    if (finalPic != null) {
-      return Image.memory(
-        finalPic!,
-        // fit: BoxFit.fitHeight,
-        width: _imageWidth,
-        height: _imageHeight,
-      );
-    }
-    return const SizedBox.shrink();
-  }
+//   buildFinalPic() {
+//     if (finalPic != null) {
+//       return Image.memory(
+//         finalPic!,
+//         // fit: BoxFit.fitHeight,
+//         width: _imageWidth,
+//         height: _imageHeight,
+//       );
+//     }
+//     return const SizedBox.shrink();
+//   }
 
-  buildStackImage() {
-    return (_profilePic != null && finalPic != null)
-        ? WidgetMask(
-            blendMode: BlendMode.srcATop,
-            childSaveLayer: true,
-            mask: Image.file(
-              _profilePic!,
-              fit: BoxFit.fill,
-            ),
-            child: Image.memory(
-              finalPic!,
-              color: Colors.black.withOpacity(1.0),
-              fit: BoxFit.fill,
-            ),
-          )
-        : const SizedBox.shrink();
-  }
+//   buildStackImage() {
+//     return (_profilePic != null && finalPic != null)
+//         ? WidgetMask(
+//             blendMode: BlendMode.srcATop,
+//             childSaveLayer: true,
+//             mask: Image.file(
+//               _profilePic!,
+//               fit: BoxFit.fill,
+//             ),
+//             child: Image.memory(
+//               finalPic!,
+//               color: Colors.black.withOpacity(1.0),
+//               fit: BoxFit.fill,
+//             ),
+//           )
+//         : const SizedBox.shrink();
+//   }
 
-  buildCameraButton() {
-    return ElevatedButton(onPressed: useCamera, child: const Text("Camera"));
-  }
+//   buildCameraButton() {
+//     return ElevatedButton(onPressed: useCamera, child: const Text("Camera"));
+//   }
 
-  buildGalleryButton() {
-    return ElevatedButton(onPressed: useGallery, child: const Text("Gallery"));
-  }
+//   buildGalleryButton() {
+//     return ElevatedButton(onPressed: useGallery, child: const Text("Gallery"));
+//   }
 
-  void useCamera() async {
-    File? cameraPicture =
-        await ImagePickerService().pickImage(ImageSource.camera) ?? _profilePic;
-    FileImage(cameraPicture!)
-        .resolve(const ImageConfiguration())
-        .addListener((ImageStreamListener((ImageInfo info, bool _) {
-          setState(() {
-            _imageWidth = info.image.width.toDouble();
-            _imageHeight = info.image.height.toDouble();
-          });
-        })));
-    finalPic = await DeepLab.predictImage(cameraPicture);
+//   void useCamera() async {
+//     File? cameraPicture =
+//         await ImagePickerService().pickImage(ImageSource.camera) ?? _profilePic;
+//     FileImage(cameraPicture!)
+//         .resolve(const ImageConfiguration())
+//         .addListener((ImageStreamListener((ImageInfo info, bool _) {
+//           setState(() {
+//             _imageWidth = info.image.width.toDouble();
+//             _imageHeight = info.image.height.toDouble();
+//           });
+//         })));
+//     finalPic = await DeepLab.predictImage(cameraPicture);
 
-    setState(() async {
-      cameraPicture ??= _profilePic;
-      _profilePic = cameraPicture;
-    });
-  }
+//     setState(() async {
+//       cameraPicture ??= _profilePic;
+//       _profilePic = cameraPicture;
+//     });
+//   }
 
-  void useGallery() async {
-    File? galleryPicture =
-        await ImagePickerService().pickImage(ImageSource.gallery) ??
-            _profilePic;
+//   void useGallery() async {
+//     File? galleryPicture =
+//         await ImagePickerService().pickImage(ImageSource.gallery) ??
+//             _profilePic;
 
-    FileImage(galleryPicture!)
-        .resolve(const ImageConfiguration())
-        .addListener((ImageStreamListener((ImageInfo info, bool _) {
-          setState(() {
-            _imageWidth = info.image.width.toDouble();
-            _imageHeight = info.image.height.toDouble();
-          });
-        })));
+//     FileImage(galleryPicture!)
+//         .resolve(const ImageConfiguration())
+//         .addListener((ImageStreamListener((ImageInfo info, bool _) {
+//           setState(() {
+//             _imageWidth = info.image.width.toDouble();
+//             _imageHeight = info.image.height.toDouble();
+//           });
+//         })));
 
-    finalPic = await DeepLab.predictImage(galleryPicture);
-    // File("test.png").writeAsBytes(finalPic!);
-    setState(() {
-      galleryPicture ??= _profilePic;
-      _profilePic = galleryPicture;
-    });
-  }
-}
+//     finalPic = await DeepLab.predictImage(galleryPicture);
+//     // File("test.png").writeAsBytes(finalPic!);
+//     setState(() {
+//       galleryPicture ??= _profilePic;
+//       _profilePic = galleryPicture;
+//     });
+//   }
+// }
